fed2c26650ceee607141abc7f9818e658e757f5b
alzhan <aismagulov@gmail.com>
2019-11-22 12:42:34 +0100

    Lesson2_Logical operators

--- app.js
+++ app.js
@@ -1,38 +1,109 @@
-const user = {
-  firstName: 'Denis',
-  age: 30,
-  isAdmin: true,
-  email: 'test@test.com',
-  'user-address': {
-    city: 'Kharkiv'
-  },
-  skills: ['html', 'css', 'js']
-};
+// >, <, >=, <=, ==, ===, !=, !==
 
 let value;
-let prop = 'skills';
 
-value = user.firstName;
-value = user['isAdmin'];
-value = user['user-address'];
-value = user['user-address'].city;
-value = user['user-address']['city'];
-value = user[prop][0];
+value = 1 > 2;
+value = 2 <= 2;
+value = 1 == 1;
+value = 1 == '1';
+value = 1 === '1';
+value = 1 != '1';
+value = 1 !== '1';
+value = 'a' > 'ab';
+value = 'A'.charCodeAt();
 
-user.firstName = 'Den';
+// console.log(value);
 
-value = user.firstName;
+// if (условие) {
+//   // actions
+// } else {
+//   // else actions
+// }
 
-user.info = 'Some info';
+value = 10;
 
-value = user.info;
+// if (value !== 10) {
+//   console.log('value: 10');
+// } else {
+//   console.log('else');
+// }
 
-user['user-address'].city = 'Kiev';
-user['user-address'].country = 'Ukraine';
+// value = 100;
 
-console.log(user.plan);
-user.plan = {};
-user.plan.basic = 'basic';
+// if (value) {
+//   console.log('some actions', value);
+// } else {
+//   console.log('else', value);
+// }
 
-console.log(value);
-console.log(user);
+// || && !
+// value = null;
+
+// if (!value) {
+//   console.log(value);
+// }
+
+// value = [1];
+
+// if (value.length) {
+//   console.log(value);
+// } else {
+//   console.log('array is empty');
+// }
+
+// value = null
+
+// if (Array.isArray(value)) {
+//   console.log(value);
+// } else {
+//   console.log('array is empty');
+// }
+
+
+// let user = {
+//   name: null
+// };
+
+// if (user.hasOwnProperty('name')) {
+//   console.log(user.name);
+// } else {
+//   console.log('else');
+// }
+
+// || &&
+// value = 0 || 0 || null;
+
+// let age = 10;
+
+// if (age < 16 || age > 65) {
+//   console.log('some actions');
+// } else {
+//   console.log('else');
+// }
+
+// let serverNickname = '';
+// let nickname = serverNickname || 'default nickname';
+
+// console.log(nickname);
+
+// value = 1 && 2 && 3;
+
+// productPrice = 10;
+
+// if (productPrice >= 5 && productPrice <= 20) {
+//   console.log('беру');
+// } else {
+//   console.log('else');
+// }
+
+// console.log(value);
+
+value = 10;
+
+if (value < 10) {
+  console.log('value < 10', value);
+} else if (value >= 10) {
+  console.log('value >= 10', value);
+} else {
+  console.log('else');
+}
