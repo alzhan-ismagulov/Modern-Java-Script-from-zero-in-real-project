3a6fdf59e5f33773291cffa8c609b199be659fad
alzhan <aismagulov@gmail.com>
2019-11-22 13:17:41 +0100

    Lesson12_Cycles

--- app.js
+++ app.js
@@ -1,27 +1,84 @@
-let a = 1;
-let b = 0;
+// while, do while, for, for of, for in
 
-// if (a > 0) {
-//   b = a;
-// } else {
-//   b += 1;
+// let i = 10;
+// while(i--) {
+//   console.log(i);
 // }
 
-// выражение ? если true : если false;
-// выражение ? если true : выражение ? если true : если false;
-b = a > 0 ? a : b + 1;
+// do {
+//   console.log('action');
+// } while(i > 0);
 
-b =  a > 0 ? 2 : a < 0 ? 3 : 0;
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   if (i === 6) {
+//     continue;
+//   }
+//   console.log(i);
+// }
 
+// let str = 'Hello';
+// let res = '';
 
-// console.log(`b: ${b}`);
-let color = 'yellow';
+// for (let i = 0; i < str.length; i++) {
+//   res += str[i] + '*';
+// }
 
-switch(color) {
-  case 'yellow': 
-  case 'red': 
-    console.log('Color is red || yellow');
-    break;
-  default: 
-    console.log('Default');
+// console.log(res);
+
+// let colors = ['white', 'black', 'yellow', 'orange'];
+
+// for (let i = 0; i < colors.length; i++) {
+//   colors[i] = colors[i].toUpperCase();
+// }
+
+// console.log(colors);
+
+const users = [
+  {
+    name: 'Denis',
+    age: 30
+  },
+  {
+    name: 'Oleg',
+    age: 12
+  },
+  {
+    name: 'Maks',
+    age: 25
+  },
+  {
+    name: 'Olga',
+    age: 2
+  },
+];
+
+const usersObj = {};
+
+for (let i = 0; i < users.length; i++) {
+  usersObj[users[i].name] = users[i];
 }
+
+// console.log(usersObj['Denis']);
+
+// const user = {
+//   name: 'Denis',
+//   age: 30
+// };
+
+// for (let key in usersObj) {
+//   console.log(key);
+//   console.log(usersObj[key]);
+// }
+
+for (let value of users) {
+  console.log(value);
+}
+
+
+
+
+
+
