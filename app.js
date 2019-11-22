05e92ba19246e71685895c2b285e51ee84dea993
alzhan <aismagulov@gmail.com>
2019-11-22 12:17:40 +0100

    Lesson8_Pattern strings

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const firstName = 'Denis';
 const lastName = 'Mescheryakov';
 const age = 30;
-const str = 'Hello my name is Denis';
 
-let value;
+let str;
 
-value = firstName + lastName;
-value = firstName + ' ' + lastName;
-value += ' I am ' + age;
+str = 'Hello my name is ' + firstName + ' ' + lastName;
 
-value = firstName.length;
-value = firstName[2];
-value = firstName[4];
-value = lastName[lastName.length - 1];
-value = firstName[firstName.length - 1];
+str = '<ul>' +
+      '<li>First name: ' + firstName + '</li>' +
+      '<li>Last name: ' + lastName + '</li>' +
+      '<li>Age: ' + age + '</li>' +
+      '</ul>';
 
-value = firstName.toUpperCase();
-value = firstName.concat(' ', lastName);
+// console.log(str);
 
-value = str.indexOf('n', 10);
-value = str.indexOf('!');
-value = str.includes('DENIS');
+// ES6
+str = `
+  <ul>
+    <li>First name: ${ firstName }</li>
+    <li>Last name: ${ lastName }</li>
+    <li>Age: ${ age }</li>
+    <li>Math.random: ${ Math.random() }</li>
+    <li>5 + 5: ${ 5 + 5 }</li>
+  </ul>
+`;
 
-value = str.slice(0, 5);
-value = str.slice(0, -3);
+document.body.innerHTML = str;
 
-value = str.replace('Denis', 'Den');
-
-
-console.log(value);
