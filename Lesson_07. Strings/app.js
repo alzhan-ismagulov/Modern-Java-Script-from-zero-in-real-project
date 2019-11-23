c504ac0500118b8b17c3fb3f9bf707c2f7450c0b
alzhan <aismagulov@gmail.com>
2019-11-23 23:51:56 +0100

    Homework8

--- Lesson_07. Strings/app.js
+++ Lesson_07. Strings/app.js
@@ -1,31 +1,30 @@
-const firstName = 'Denis';
-const lastName = 'Mescheryakov';
+const firstName = "Denis";
+const lastName = "Mescheryakov";
 const age = 30;
-const str = 'Hello my name is Denis';
+const str = "Hello my name is Denis";
 
 let value;
 
 value = firstName + lastName;
-value = firstName + ' ' + lastName;
-value += ' I am ' + age;
+value = firstName + " " + lastName;
+value += " I am " + age;
 
-value = firstName.length;
-value = firstName[2];
-value = firstName[4];
-value = lastName[lastName.length - 1];
-value = firstName[firstName.length - 1];
+// value = firstName.length; //Выводит 5 - количество знаков в строке first name
+// value = firstName[2]; //n - в имени с индексом 2 стоит буква n
+// value = firstName[4];
+// value = lastName[lastName.length - 1]; //v
+// value = firstName[firstName.length - 1];
 
-value = firstName.toUpperCase();
-value = firstName.concat(' ', lastName);
+// value = firstName.toUpperCase();
+// value = firstName.concat(" ", lastName);
 
-value = str.indexOf('n', 10);
-value = str.indexOf('!');
-value = str.includes('DENIS');
+// value = str.indexOf("n", 10); //19 позиция буквы
+// value = str.indexOf("!");// -1
+// value = str.includes("DENIS");
 
-value = str.slice(0, 5);
-value = str.slice(0, -3);
+// value = str.slice(0, 5); //Hello
+value = str.slice(0, -3); // Hello my name is De
 
-value = str.replace('Denis', 'Den');
-
+// value = str.replace("Denis", "Den"); //Замена строки
 
 console.log(value);
