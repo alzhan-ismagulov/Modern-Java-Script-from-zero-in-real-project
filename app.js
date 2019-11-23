3c983a70e9b355aaae1a0de5c81ed9d4085d6945
alzhan <aismagulov@gmail.com>
2019-11-23 11:54:29 +0100

    Lesson17_Arrow functions

--- app.js
+++ app.js
@@ -1,70 +1,37 @@
-function getThis() {
-  console.log(this);
-}
-// getThis();
-// window.getThis();
-// console.log(window.getThis);
+const plus = (x = 0, y = 0) => x + y;
+const plusRes = plus(1, 2);
 
-function getPrice(currency = '$') {
-  console.log(currency + this.price);
-  return this;
+const withoutArgs = () => console.log('Hello world');
+const singleArg = x => x * 2;
+const moreActions = (a, b) => {
+  a *= 2;
+  b *= 3;
+  return a + b;
+};
+const returnObj = (str = '') => ({
+  value: str,
+  length: str.length,
+});
+
+function plusFoo(x, y) {
+  console.log(arguments);
+  return x + y;
 }
 
-function getName() {
-  console.log(this.name);
-  return this;
-}
+// plusFoo(1, 2, 3, 'hello');
 
-const prod1 = {
-  name: 'Intel',
-  price: 100,
-  getPrice,
-  getName() {
-    console.log(this.name);
+const obj = {
+  firstName: 'Denis',
+  age: 30,
+  getFirstName() {
+    console.log(this);
   },
-  info: {
-    information: ['2.3ghz'],
-    getInfo: function() {
-      console.log(this);
-    },
+  getAgeArrow: null,
+  getAge() {
+    // this.getAgeArrow = () => console.log(this);
+    setTimeout(() => console.log(this));
   },
 };
-// prod1.getPrice();
-// prod1.info.getInfo();
-// prod1.getName();
 
-const prod2 = {
-  name: 'AMD',
-  price: 50,
-  getPrice,
-};
-
-prod2.getName = prod1.getName;
-// prod2.getPrice();
-
-// prod2.getName();
-
-let str = 'Hello world';
-const reversStr = str
-  .split('') // ['H', 'e'...]
-  .reverse() // ['d', 'l'...]
-  .join(''); // 'dlrow olleH'
-// console.log(reversStr);
-
-const prod3 = {
-  name: 'ARM',
-  price: 200,
-  getPrice,
-  // getName,
-};
-
-// getPrice.call(prod3, '*');
-// getPrice.apply(prod3, ['*']);
-
-// prod3
-//   .getName() // prod3
-//   .getPrice(); // prod3.getPrice()
-
-const getPriceBind = prod3.getPrice.bind(prod3, '*');
-console.log(getPriceBind);
-setTimeout(getPriceBind, 1000);
+obj.getAge();
+// obj.getAgeArrow();
